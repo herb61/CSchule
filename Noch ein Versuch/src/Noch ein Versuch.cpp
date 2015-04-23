f1e3dbe10da05889c8e0ce0d2182d570ba25495f
herb61 <herbert.pichler@vigaun.info>
2015-04-23 11:38:14 -0700

    ein kommentar

--- Noch ein Versuch/src/Noch ein Versuch.cpp
+++ Noch ein Versuch/src/Noch ein Versuch.cpp
@@ -7,9 +7,11 @@
 //============================================================================
 
 #include <iostream>
+
 using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	//
 	return 0;
 }
