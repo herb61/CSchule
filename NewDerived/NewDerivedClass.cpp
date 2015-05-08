34e31d3e821668baae97293378c3ee4950d92b17
herb61 <herbert.pichler@vigaun.info>
2015-05-08 11:33:25 -0700

    Test

---
+++ NewDerived/NewDerivedClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * NewDerivedClass.cpp
+ *
+ *  Created on: May 8, 2015
+ *      Author: schueler
+ */
+
+#include "NewDerivedClass.h"
+
+NewDerivedClass::NewDerivedClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NewDerivedClass::~NewDerivedClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ NewDerived/NewDerivedClass.h
@@ -0,0 +1,20 @@
+/*
+ * NewDerivedClass.h
+ *
+ *  Created on: May 8, 2015
+ *      Author: schueler
+ */
+
+#ifndef NEWDERIVEDCLASS_H_
+#define NEWDERIVEDCLASS_H_
+
+class NewDerivedClass {
+private:
+	char* newName;
+	int test;
+public:
+	NewDerivedClass();
+	virtual ~NewDerivedClass();
+};
+
+#endif /* NEWDERIVEDCLASS_H_ */
